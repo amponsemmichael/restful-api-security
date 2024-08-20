039f97e31edf354edb4367831968192ac2fa6932
amponsemmichael <amponsem_michael@yahoo.com>
2024-08-20 10:51:58 +0000

    initial project setup

---
+++ restful-api-security/src/main/java/com/amponsem/restful_api_security/RestfulApiSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.amponsem.restful_api_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiSecurityApplication.class, args);
+	}
+
+}
---
+++ restful-api-security/src/test/java/com/amponsem/restful_api_security/RestfulApiSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amponsem.restful_api_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
