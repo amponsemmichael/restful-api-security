9d982307b5f98cb0f44e963c84e505ce38286e14
amponsemmichael <amponsem_michael@yahoo.com>
2024-08-21 20:31:18 +0000

    updated the system to include input validation

--- restful-api-security/src/main/java/com/amponsem/restful_api_security/model/Users.java
+++ restful-api-security/src/main/java/com/amponsem/restful_api_security/model/Users.java
@@ -1,14 +1,16 @@
 package com.amponsem.restful_api_security.model;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Entity
 @Data
@@ -19,6 +21,15 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank(message = "Username is mandatory")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    @Email(message = "Username must be a valid email address")
+    //@Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can only contain alphanumeric characters and underscores")
     private String username;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
 }
